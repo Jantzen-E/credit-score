26fb05e7f2940337b90ccc7f8257ce57d255a3d7
Jantzen-E <jantzen.egan@gmail.com>
2020-04-09 21:59:51 -0600

    add footer

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import FactorList from './FactorList';
 import '../Responsive.css';
+import Footer from './Footer';
 
 //This is a stateful component example in case I want to add state and a backend to this react app in the future
 
@@ -25,6 +26,7 @@ function Page() {
         <div className="page">
             <h1>What factors affect your credit score?</h1>
             <FactorList />
+            <Footer />
         </div>
     );
 }
