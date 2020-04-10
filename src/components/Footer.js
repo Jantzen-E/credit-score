6da8233e1a82fd68a6b3e66282a4a68a47d14ce3
Jantzen-E <jantzen.egan@gmail.com>
2020-04-09 22:10:56 -0600

    make footer a functional component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import '../Responsive.css';
 
-class Footer extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+// class Footer extends React.Component {
+//     constructor(props) {
+//         super(props);
+//     }
 
-    render() {
-        return(
-            <div className="footer">
-                <h5 className="footerTitle">Jantzen Egan 2020</h5>
-                <a href="https://github.com/Jantzen-E"><span className="githubLink">Github</span></a>
-                <a href="https://www.linkedin.com/in/j-egan-/"><span className="linkedInLink">Linkedin</span></a>
-            </div>
-        );   
-    }
+function Footer() {
+    return (
+        <div className="footer">
+            <h5 className="footerTitle">Jantzen Egan 2020</h5>
+            <a href="https://github.com/Jantzen-E"><span className="githubLink">Github</span></a>
+            <a href="https://www.linkedin.com/in/j-egan-/"><span className="linkedInLink">Linkedin</span></a>
+        </div>
+    );   
 }
 
 export default Footer;
